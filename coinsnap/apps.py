1ab037d35fba9b7cd4f95b02864842a2abed07a5
JovanGH <116644739+JovanGH@users.noreply.github.com>
2025-02-02 12:23:18 +0100

    Remove leading "/" for logo

--- coinsnap/apps.py
+++ coinsnap/apps.py
@@ -18,7 +18,7 @@ class PluginApp(PluginConfig):
         description = gettext_lazy("Accept Bitcoin-Lightning for your event with this easy-to-install, and easy-to mange, safe and secure plugin. Receive your payments in real-time, directly into your Lightning wallet, without any risk of fraud or charge-back. Coinsnap is the leading provider of software for Bitcoin-Lightning payment directly from your customer’s wallet into your wallet, and Coinsnap charges just 1,25% (or 1% with Referrer code) of the payment. By the way: Coinsnap never touches your Bitcon - we only facilitate the transfer from your customer to you.")
         visible = True
         version = '0.1.0'
-        picture = "/coinsnap/logo.png"
+        picture = "coinsnap/logo.png"
         category = "PAYMENT"
         compatibility = "pretix>=2.7.0"
 
